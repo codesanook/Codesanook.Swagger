3ace6680deaf0950a8f95e4605dc883baf54e415
aaronamm <theeranitp@gmail.com>
2017-10-24 22:35:23 +0700

    added Swagger resource to CodeSanook.Swagger module

---
+++ Controllers/SwaggerUiController.cs
@@ -0,0 +1,29 @@
+﻿using Orchard.DisplayManagement;
+using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaptureTheFracture.User.Controllers
+{
+    //[Themed]
+    public class SwaggerUiController : Controller
+    {
+        private readonly IShapeFactory shapeFactory;
+
+        public SwaggerUiController(IShapeFactory shapeFactory)
+        {
+            this.shapeFactory = shapeFactory;
+        }
+
+
+        // GET: SwaggerUi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ResourceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.UI.Resources;
+
+namespace CodeSanook.FacebookConnect {
+    public class ResourceRegistration: IResourceManifestProvider  {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+
+            var manifest = builder.Add();
+            //manifest.DefineScript("AngularJs").SetUrl("angular.min.js", "angular.js")
+            //    .SetVersion(angularVersion);
+
+            var  imagePath = "~/Modules/Modules/Orchard.Resources/Styles/images";
+            manifest.DefineResource("image", "logo-small").SetBasePath(imagePath).SetUrl("logo_small.png");
+
+        }
+    }
+}
