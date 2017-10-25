31e300a277bf1b28d8274c985a9ab100cbe4750a
aaronamm <theeranitp@gmail.com>
2017-10-25 07:10:03 +0700

    added Swagger routes and update projects to have JSON.NET 10.0.3

--- ResourceRegistration.cs
+++ ResourceRegistration.cs
@@ -5,9 +5,6 @@ namespace CodeSanook.FacebookConnect {
         public void BuildManifests(ResourceManifestBuilder builder) {
 
             var manifest = builder.Add();
-            //manifest.DefineScript("AngularJs").SetUrl("angular.min.js", "angular.js")
-            //    .SetVersion(angularVersion);
-
             var  imagePath = "~/Modules/Modules/Orchard.Resources/Styles/images";
             manifest.DefineResource("image", "logo-small").SetBasePath(imagePath).SetUrl("logo_small.png");
 
---
+++ Routes.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace CodeSanook.Swagger
+{
+    public class Routes : IRouteProvider
+    {
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+
+                new RouteDescriptor {
+                    Name = "SwaggerUI",
+                    Priority = 100,
+                    Route = new Route(
+                        "swagger",
+                        new RouteValueDictionary {
+                            {"area", "CodeSanook.Swagger"},
+                            {"controller", "Swagger"},
+                            {"action", "Index"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary {
+                            {"area", "CodeSanook.Swagger"}
+                        },
+                        new MvcRouteHandler())
+                },
+
+                new RouteDescriptor {
+                    Name = "SwaggerDoc",
+                    Priority = 9,
+                    Route = new Route(
+                        "swagger/doc/{version}",
+                        new RouteValueDictionary {
+                            {"area", "CodeSanook.Swagger"},
+                            {"controller", "Swagger"},
+                            {"action", "GenerateDocument"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary {
+                            {"area", "CodeSanook.Swagger"}
+                        },
+                        new MvcRouteHandler())
+                }
+            };
+        }
+    }
+}
