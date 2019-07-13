f6ae51bc46d642905b24feea274c72e17a197a92
aaronamm <theeranitp@gmail.com>
2019-07-13 12:38:40 +0700

    Update AssemblyTitle

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,10 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeSanook.Swagger")]
+[assembly: AssemblyTitle("Codesanook.Swagger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Orchard")]
